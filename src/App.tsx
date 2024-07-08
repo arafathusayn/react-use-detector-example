4f8ed3567e76d5bc545b1d261b12227f667bc8ec
Mettle X <56449790+mettlex@users.noreply.github.com>
2024-07-08 20:54:07 +0600

    chore: Add configureWithTimeout function and text input components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useValue } from "../../src/index"
+import { configureWithTimeout, useValue } from "../../src/index"
+
+configureWithTimeout()
 
 let count = 0
 
@@ -14,9 +16,36 @@ function DoubleCount() {
   return <p>{count * 2}</p>
 }
 
+const state = {
+  textInput: "",
+}
+
+function TextInput() {
+  useValue(() => state.textInput)
+
+  return (
+    <input
+      type="text"
+      value={state.textInput}
+      onChange={(e) => {
+        state.textInput = e.target.value
+      }}
+    />
+  )
+}
+
+function DisplayText() {
+  useValue(() => state.textInput)
+
+  return <p>{state.textInput}</p>
+}
+
 export default function App() {
   return (
     <div>
+      <TextInput />
+      <DisplayText />
+
       <Count />
       <DoubleCount />
       <button onClick={() => count++}>Increment</button>
